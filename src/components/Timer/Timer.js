886c6ad160484874d97b78f43f99a7f0248675a8
Agnieszka Dawidowicz <agnieszka.dawidowicz.com>
2023-05-19 14:15:23 +0200

    Removed slice method inside Timer component

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,21 +1,16 @@
 import styles from "./Timer.module.scss";
+
 export const Timer = ({ time }) => {
   const convertTime = (miliseconds) => {
-    const hh = String(Math.floor((miliseconds / (60 * 60 * 1000)) % 60))
-      .padStart(2, "0")
-      .slice(-2);
-    const mm = String(Math.floor((miliseconds / (60 * 1000)) % 60))
-      .padStart(2, "0")
-      .slice(-2);
-    const ss = String(Math.floor((miliseconds / 1000) % 60))
-      .padStart(2, "0")
-      .slice(-2);
-    const mil = String(Math.floor(miliseconds % 1000))
-      .padStart(2, "0")
-      .padStart(3, "0")
-      .slice(-3);
+    const hh = String(
+      Math.floor((miliseconds / (60 * 60 * 1000)) % 60)
+    ).padStart(2, "0");
+    const mm = String(Math.floor((miliseconds / (60 * 1000)) % 60)).padStart(
+      2,
+      "0"
+    );
+    const ss = String(Math.floor((miliseconds / 1000) % 60)).padStart(2, "0");
+    const mil = String(Math.floor(miliseconds % 1000)).padStart(3, "0");
 
     return `${hh}:${mm}:${ss}.${mil}`;
   };
-  return <div className={styles.timer}>{convertTime(time)}</div>;
-};
